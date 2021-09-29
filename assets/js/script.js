1c9d2c8fa73ca9fc190b083d57f6f53eb002d936
Doss Anania <dossanania@gmail.com>
2021-09-30 03:12:40 +1000

    script for last comit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,20 @@
 var searchInput = document.querySelector("#search")
 var submitEl = document.querySelector("#form")
 var cityList = document.querySelector("#prev-search-list")
+
+// current weather elements
+var tempEl = document.querySelector("#temp-current")
+var windEl = document.querySelector("#wind-current")
+var humidityEl = document.querySelector("#hum-current")
+var uvEl = document.querySelector("#uvi-box")
+
 var apiKey = 'ffb7e55f593d9cc120525edbd6e94c9e'
 var weatherAppCityList = 'weatherAppCityList';
 
 // render from local storage
 function renderCityList(updatedCityArray) {
   cityList.innerHTML = ''
-  for (i = 0; i < updatedCityArray.length; i++) {
+  for (i = updatedCityArray.length - 1; i >= 0; i--) {
     var tempItem = updatedCityArray[i];
 
     var li = document.createElement('li');
@@ -36,12 +43,24 @@ function saveCitySearch(name, country, coord) {
     lon: coord.lon
   }
 
-
   var storedCityArray = getLocalStorage();
-// no check for max length or duplicates
+  // var duplicateChecker = storedCityArray.some(function (cityObj2) {return cityObj2.lat === cityObj.lat && cityObj2.lon === cityObj.lon})
+  var targetIndex = -1;
+  for (i = 0; i < storedCityArray.length; i++) {
+    if (storedCityArray[i].lat === cityObj.lat && storedCityArray[i].lon === cityObj.lon) {
+      targetIndex = i; 
+      break     
+    }
+  }
+  if (targetIndex >= 0) { 
+  storedCityArray.splice(targetIndex, 1);
+  }
+  // if (!duplicateChecker) {
+  // }
   storedCityArray.push(cityObj)
   localStorage.setItem(weatherAppCityList, JSON.stringify(storedCityArray))
   renderCityList(storedCityArray)
+  // no check for max length or duplicates
 }
 
 // API search
@@ -60,9 +79,11 @@ function findCity(searchInputVal) {
       locRes['sys']['country'],
       locRes['coord']
     );
+    weatherCall();
   })
   .catch(function (error) {
     console.error(error);
+    renderCurrentStats
   })    
     
 }
@@ -71,11 +92,27 @@ function findCity(searchInputVal) {
 // temp, wind speed, humidity, UV i
 // current[humidity], current[temp]
 // wind[speed]
-// 
-  
-function renderCurrentStats(params) {
+function renderCurrentStats(weatherData) {
+  tempEl.innerHTML = weatherData.temp;
+  windEl.innerHTML = weatherData.wind_speed;
+  humidityEl.innerHTML = weatherData.humidity;
+  // 1-2, 3-5, 8-10
+  var uvi = weatherData.uvi
+  uvEl.innerHTML = uvi;
+  uvEl.removeAttribute('class')
+    if (uvi < 3) {
+      uvEl.classList.add('uvi-box-green')
+    } else if (uvi < 6) {
+      uvEl.classList.add('uvi-box-yellow')
+    } else if (uvi < 11) {
+      uvEl.classList.add('uvi-box-red')
+    }
 
 }
+function renderForercast() {
+
+}
+
 
 function weatherCall() {
   var storedCityArray = getLocalStorage();
@@ -93,7 +130,7 @@ function weatherCall() {
     return response.json();
   })
   .then(function (locRes) {
-    renderCurrentStats(locRes);
+    renderCurrentStats(locRes.current);
     renderForercast()
   })
   .catch(function (error) {
@@ -111,7 +148,6 @@ function citySearchHandler(event) {
     console.error('You need a search input value!'); 
   } else {
     findCity(searchInput.value)
-    weatherCall();
   }
 }
 
